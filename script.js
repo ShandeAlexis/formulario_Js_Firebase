99c7242a66f69108afd45d02b47c024f60694eb6
Alexis Diaz <alexisshande@gmail.com>
2024-09-17 11:48:51 -0500

    V1.0.2

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
-document.getElementById('formulario').addEventListener('submit', (event) => {
+const firebaseConfig = {
+  apiKey: "AIzaSyBYdj9EU2NveugnVkfbBG_DYN8tEjOy_mw",
+  authDomain: "fomulario-datos.firebaseapp.com",
+  projectId: "fomulario-datos",
+  storageBucket: "fomulario-datos.appspot.com",
+  messagingSenderId: "544279473353",
+  appId: "1:544279473353:web:db89cbbe91382195dfb118",
+  measurementId: "G-EMQJGJF2SE",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db=firebase.firestore();
+
+document.getElementById("formulario").addEventListener("submit", (event) => {
   event.preventDefault();
 
-  let entradaNombre = document.getElementById('name');
-  let errorNombre = document.getElementById('nameError');
-  if (entradaNombre.value.trim() === '') {
-    errorNombre.textContent = 'Por favor , introduce tu nombre';
-    errorNombre.classList.add('error-message');
+  let entradaNombre = document.getElementById("name");
+  let errorNombre = document.getElementById("nameError");
+  if (entradaNombre.value.trim() === "") {
+    errorNombre.textContent = "Por favor , introduce tu nombre";
+    errorNombre.classList.add("error-message");
   } else {
-    errorNombre.textContent = '';
-    errorNombre.classList.remove('error-message');
+    errorNombre.textContent = "";
+    errorNombre.classList.remove("error-message");
   }
 
-  let emailEntrada = document.getElementById('email');
-  let emailError = document.getElementById('emailError');
+  let emailEntrada = document.getElementById("email");
+  let emailError = document.getElementById("emailError");
   let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Patrón de validación básico
 
   if (!emailPattern.test(emailEntrada.value)) {
-    emailError.textContent = 'Por favor , introduce un email valido';
-    emailError.classList.add('error-message');
+    emailError.textContent = "Por favor , introduce un email valido";
+    emailError.classList.add("error-message");
   } else {
-    emailError.textContent = '';
-    emailError.classList.remove('error-message');
+    emailError.textContent = "";
+    emailError.classList.remove("error-message");
   }
 
-  let contraseñaEntrada = document.getElementById('password');
-  let contraseñaError = document.getElementById('passwordError');
-  let contraseñaPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/
+  let contraseñaEntrada = document.getElementById("password");
+  let contraseñaError = document.getElementById("passwordError");
+  let contraseñaPattern =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&#.$($)$-$_])[A-Za-z\d$@$!%*?&#.$($)$-$_]{8,15}$/;
 
   if (!contraseñaPattern.test(contraseñaEntrada.value)) {
-    contraseñaError.textContent = 'Por favor , introduce mas de 8 caracteres, numeros , mayuscula y minuscula y caracteres especiales.';
-    contraseñaError.classList.add('error-message');
+    contraseñaError.textContent =
+      "Por favor , introduce mas de 8 caracteres, numeros , mayuscula y minuscula y caracteres especiales.";
+    contraseñaError.classList.add("error-message");
   } else {
-    contraseñaError.textContent = '';
-    contraseñaError.classList.remove('error-message');
+    contraseñaError.textContent = "";
+    contraseñaError.classList.remove("error-message");
   }
 
   if (
@@ -40,7 +56,17 @@ document.getElementById('formulario').addEventListener('submit', (event) => {
     !emailError.textContent &&
     !contraseñaError.textContent
   ) {
-    alert('El formulario se envio correctamente!');
-    document.getElementById('formulario').reset();
+    db.collection("users").add({
+        nombre: entradaNombre.value,
+        email: emailEntrada.value,
+        password: contraseñaEntrada.value,
+      })
+      .then((docRef) => {
+        alert("El formulario se envio correctamente!", docRef);
+        document.getElementById("formulario").reset();
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
   }
 });
